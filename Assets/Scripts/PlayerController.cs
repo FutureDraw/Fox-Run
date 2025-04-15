d3a34a03096852e817912aa5ad7ae61bc1782013
Khanlimon29 <flick1@list.ru>
2025-04-16 00:52:38 +0300

    add double jump and new comments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 
+/// <summary>
+/// Класс для описания поведения игровой модели персонажа
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public float jumpForce = 5f;
+
     private Rigidbody2D rb;
     private bool isGrounded;
+    private bool canDoubleJump;
+
 
     [Header("Ground Check Settings")]
     public Transform groundCheck;
     public float checkRadius = 0.1f;
     public LayerMask whatIsGround;
+
+    private float moveX;
+    private int facingDirection = 1;
 
     private void Start()
     {
@@ -19,16 +28,39 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+        moveX = Input.GetAxisRaw("Horizontal");
 
-        // Проверяем касание снизу
+        // Обновление направления взгляда
+        if (moveX != 0)
+            facingDirection = (int)Mathf.Sign(moveX);
+
+        GroundCheck();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                canDoubleJump = true;
+            }
+            //второй прыжок
+            else if (canDoubleJump)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+                canDoubleJump = false;
+            }
+        }
+    }
+    // <summary>
+    // проверка на столкновение с землей
+    // </summary>
+    private void GroundCheck()
+    {
         Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, checkRadius, whatIsGround);
         isGrounded = false;
 
         foreach (Collider2D col in colliders)
         {
-            // Проверяем, что касание идёт СНИЗУ (а не сбоку или сверху)
             Vector2 contactPoint = col.ClosestPoint(transform.position);
             if (contactPoint.y < transform.position.y - 0.1f)
             {
@@ -37,12 +69,13 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
+        if (isGrounded)
+            canDoubleJump = true;
     }
 
+    /// <summary>
+    /// Debug отрисовка гизмоса
+    /// </summary>
     private void OnDrawGizmosSelected()
     {
         if (groundCheck != null)
