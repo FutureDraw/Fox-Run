2a515423b3221bfb58457ec2f765ad4094ec6a78
Khanlimon29 <flick1@list.ru>
2025-04-25 14:03:28 +0300

    add basic     ё  ж  и  к      logic

---
+++ Assets/Scripts/Traps/JogikController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class JogikController : MonoBehaviour
+{
+    [SerializeField] public GameObject PlaceToTp; // пустой объект к которому переместит игрока после "смерти"
+    [SerializeField] AudioSource killSound;
+    [SerializeField] public float rotationSpeed = 500;
+    public void Start()
+    {
+        GetComponent<Collider2D>().isTrigger = true;
+        Debug.Log("Jogik initialized and armed");
+    }
+
+    public void Update()
+    {
+        transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+    }
+
+
+    //<Summary>
+    // Не реализовано
+    //</Summary>
+    public void StopPlayer(float time)
+    {
+    }
+
+    //<Summary>
+    // Не реализовано
+    //</Summary>
+    public void SlowPlayer(float time, float strength)
+    {
+    }
+
+    //<Summary>
+    //
+    //</Summary>
+    public void KillPlayer()
+    {
+        var player = FindObjectOfType<PlayerController>();
+        if (player != null)
+        {
+            player.transform.position = PlaceToTp.transform.position;
+            killSound.Play(0);
+            Debug.Log($"Player killed >:D");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("collision");
+        if (other.CompareTag("Player"))
+        {  
+            KillPlayer();
+        }
+    }
+}
