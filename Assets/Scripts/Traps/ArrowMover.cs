6f9054a11f1f85fea24b0ba375203c4bd8b854d0
Khanlimon29 <flick1@list.ru>
2025-04-27 22:44:58 +0300

    add basic arrow dispencer

---
+++ Assets/Scripts/Traps/ArrowMover.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+/// <Summary>
+/// Управляет движением стрелы по прямой,
+/// уничтожает по таймауту или при столкновении.
+/// </Summary>
+public class ArrowMover : MonoBehaviour
+{
+    private Vector2 _direction;
+    private float _speed;
+    private float _lifetime;
+
+    /// <Summary>
+    /// Инициализация параметров полёта стрелы
+    /// </Summary>
+    public void Initialize(Vector2 direction, float speed, float lifetime)
+    {
+        _direction = direction.normalized;
+        _speed = speed;
+        _lifetime = lifetime;
+        // Уничтожаем стрелу через _lifetime секунд
+        Destroy(gameObject, _lifetime);
+    }
+
+    private void Update()
+    {
+        // Двигаем стрелу вперёд
+        transform.Translate(_direction * _speed * Time.deltaTime, Space.World);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // При любом столкновении уничтожаем стрелу
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject, 0f);
+    }
+
+}
