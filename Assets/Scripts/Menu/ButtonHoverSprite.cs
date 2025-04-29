1ac8f633703b76c95940f78cbabc7bfb99035aa4
FutureDraw <avdeevchope@gmail.com>
2025-04-29 21:39:41 +0300

    Скрипт для смены спрайта при наведении мышки

---
+++ Assets/Scripts/Menu/ButtonHoverSprite.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonHoverSprite : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Sprite normalSprite;
+    public Sprite hoverSprite;
+
+    private Image buttonImage;
+
+    void Start()
+    {
+        buttonImage = GetComponent<Image>();
+        if (buttonImage != null && normalSprite != null)
+        {
+            buttonImage.sprite = normalSprite;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (buttonImage != null && hoverSprite != null)
+        {
+            buttonImage.sprite = hoverSprite;
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (buttonImage != null && normalSprite != null)
+        {
+            buttonImage.sprite = normalSprite;
+        }
+    }
+}
