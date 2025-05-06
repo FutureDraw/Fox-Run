7d3a854badfc0c3ff1d92b88745f6928efbad46f
FutureDraw <avdeevchope@gmail.com>
2025-05-06 19:07:05 +0300

    Добавил затемнение при выборе вкладки

--- Assets/Scripts/Menu/SettingsTabs.cs
+++ Assets/Scripts/Menu/SettingsTabs.cs
@@ -7,18 +7,20 @@ public class SettingsTabs : MonoBehaviour
     public GameObject displayPanel;
     public GameObject controlsPanel;
 
-    public void OnClickSettingsButton()
-    {
-        audioPanel.SetActive(false);
-        displayPanel.SetActive(false);
-        controlsPanel.SetActive(true);
-    }
+    public Image audioButtonImage;
+    public Image displayButtonImage;
+    public Image controlsButtonImage;
+
+    public float activeAlpha = 0.6f;  // Затемнённая кнопка (например, 150/255)
+    public float inactiveAlpha = 0f;
 
     public void ShowAudio()
     {
         audioPanel.SetActive(true);
         displayPanel.SetActive(false);
         controlsPanel.SetActive(false);
+
+        UpdateTabTransparency(audioButtonImage);
     }
 
     public void ShowDisplay()
@@ -26,6 +28,8 @@ public class SettingsTabs : MonoBehaviour
         audioPanel.SetActive(false);
         displayPanel.SetActive(true);
         controlsPanel.SetActive(false);
+
+        UpdateTabTransparency(displayButtonImage);
     }
 
     public void ShowControls()
@@ -33,5 +37,23 @@ public class SettingsTabs : MonoBehaviour
         audioPanel.SetActive(false);
         displayPanel.SetActive(false);
         controlsPanel.SetActive(true);
+
+        UpdateTabTransparency(controlsButtonImage);
+    }
+
+    private void UpdateTabTransparency(Image activeImage)
+    {
+        SetAlpha(audioButtonImage, inactiveAlpha);
+        SetAlpha(displayButtonImage, inactiveAlpha);
+        SetAlpha(controlsButtonImage, inactiveAlpha);
+
+        SetAlpha(activeImage, activeAlpha);
+    }
+
+    private void SetAlpha(Image img, float alpha)
+    {
+        Color c = img.color;
+        c.a = alpha;
+        img.color = c;
     }
 }
