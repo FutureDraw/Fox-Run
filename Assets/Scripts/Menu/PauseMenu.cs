4bcfe6879eabe447230f06ab6e58b978793f7ddc
FutureDraw <avdeevchope@gmail.com>
2025-05-08 22:32:03 +0300

    Написал скрипт управления меню паузы

---
+++ Assets/Scripts/Menu/PauseMenu.cs
@@ -0,0 +1,91 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public GameObject settingsPanel;
+    private bool isPaused = false;
+
+#if UNITY_EDITOR
+    public SceneAsset sceneAsset;
+#endif
+
+    public float delayBeforeSceneLoad = 0.5f;
+
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (settingsPanel.activeSelf)
+            {
+                settingsPanel.SetActive(false);
+                pauseMenuUI.SetActive(true);
+            }
+            else if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        settingsPanel.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    private IEnumerator WaitAndLoadScene()
+    {
+        Time.timeScale = 1f;
+        yield return new WaitForSeconds(delayBeforeSceneLoad);
+
+#if UNITY_EDITOR
+        if (sceneAsset != null)
+        {
+            string sceneName = sceneAsset.name;
+            SceneManager.LoadScene(sceneName);
+        }
+#endif
+    }
+
+    public void OnSettingsClicked()
+    {
+        pauseMenuUI.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void OnExitClicked()
+    {
+        StartCoroutine(WaitAndLoadScene());
+    }
+}
