66c79e3890c9446c9e2def2ba95d671a0ef1f0f2
FutureDraw <avdeevchope@gmail.com>
2025-05-11 05:18:12 +0300

    Изменил скрипт для биндов во время игры
    Сделал возможность выходить из ребинда на esc и лкм

--- Assets/Samples/Input System/1.7.0/Rebinding UI/RebindActionUI.cs
+++ Assets/Samples/Input System/1.7.0/Rebinding UI/RebindActionUI.cs
@@ -269,11 +269,17 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
                 m_RebindOperation = null;
             }
 
+            action.Disable();
+
             // Configure the rebind.
             m_RebindOperation = action.PerformInteractiveRebinding(bindingIndex)
+                .WithControlsExcluding("<Mouse>")
+                .WithControlsExcluding("<Mouse>/leftbutton")
+                .WithCancelingThrough("<Keyboard>/escape")
                 .OnCancel(
                     operation =>
                     {
+                        action.Enable();
                         m_RebindStopEvent?.Invoke(this, operation);
                         m_RebindOverlay?.SetActive(false);
                         UpdateBindingDisplay();
@@ -283,6 +289,7 @@ namespace UnityEngine.InputSystem.Samples.RebindUI
                 .OnComplete(
                     operation =>
                     {
+                        action.Enable();
                         m_RebindOverlay?.SetActive(false);
                         m_RebindStopEvent?.Invoke(this, operation);
                         UpdateBindingDisplay();
